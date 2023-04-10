4a9c604e56b3398c1593b4b32eb6910e7600297c
wyzbel <119756165+wyzbel@users.noreply.github.com>
2023-04-10 15:46:30 +0200

    Create pie-simulation.py

---
+++ pie-simulation.py
@@ -0,0 +1,26 @@
+#for this program we use monte carlo simulation to approximate pie.
+#The large number of dots you enter, the better the approximation get.
+import random
+import math
+
+flag = True
+while flag == True:
+    all_dots = int(input("Enter the dots to approximate pie:"))
+    dots_inside_circle = 0
+    radius = 1
+    
+    for i in range(all_dots):
+        x = random.uniform(0,1)
+        y = random.uniform(0,1)
+        if x**2 + y**2 <= radius**2:
+            dots_inside_circle += 1 
+        pie_approximation = 4*(dots_inside_circle/all_dots)
+        
+    print()
+    print("The approximation of pie at ",i+1,"is:",pie_approximation)
+    pie_value = math.pi
+    error_percentage = ((pie_value - pie_approximation)/pie_approximation)*100
+    print()
+    print("The real value of pie is:",pie_value)
+    print()
+    print("The error percentage is :", error_percentage)
